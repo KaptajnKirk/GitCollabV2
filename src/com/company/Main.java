0c24bc47bb107649a671f41ad7902125e93f9e3f
Linding3r <thomasjuullindinger@yahoo.dk>
2022-03-11 13:54:14 +0100

    test2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,21 +2,20 @@ package com.company;
 
 public class Main {
 
-
+    public void test2(){
+        System.out.println("Theo er n00b");
+    }
     public void test () {
 
         System.out.println("Thomas er ikke CEO, han er rengøringsdame");
     }
 
+
     public static void main(String[] args) {
 
         Main obj = new Main();
         obj.test();
-
-
-
+        obj.test2();
 
     }
-
-
 }
