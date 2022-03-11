7d76ddd47c14c6987d204d42f089ce1d19926380
KaptajnKirk <theodor@duus-kirk.dk>
2022-03-11 14:01:47 +0100

    Test4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,11 @@
 package com.company;
 
 public class Main {
+
+
+    public void test4 (){
+        System.out.println("Reaper Leviathan just wants a kiss");
+    }
     public void test3(){
         System.out.println("Tester3");
     }
@@ -23,8 +28,7 @@ public class Main {
         obj.test();
         obj.test2();
         obj.tester();
-
-        Main obj = new Main();
         obj.test3();
+        obj.test4();
     }
 }
