6ee8ed3aaccd63c03ac58b579dd2b87996b46467
morten-l-k <mort75q3@stud.kea.dk>
2022-03-11 13:55:49 +0100

    Tester - second collaborator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,16 @@
 package com.company;
 
 public class Main {
+    public void test3(){
+        System.out.println("Tester3");
+    }
 
     public static void main(String[] args) {
 
         System.out.println("Hva så, røvhuller?");
         System.out.println("Fuckedi Fuck Fuck Fuck");
 
+        Main obj = new Main();
+        obj.test3();
     }
 }
